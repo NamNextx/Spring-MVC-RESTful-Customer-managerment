2f90edd6c26a0cea6115ebea5ce826919aa5389d
Namnextx <phuongnam.nextx@gmail.com>
2019-05-08 15:07:16 +0700

    Update repository

---
+++ src/main/java/com/codegym/cms/repository/impl/CustomerReposytoryImpl.java
@@ -0,0 +1,28 @@
+package com.codegym.cms.repository.impl;
+
+import com.codegym.cms.model.Customer;
+import com.codegym.cms.repository.CustomerReposytory;
+
+import java.util.List;
+
+public class CustomerReposytoryImpl implements CustomerReposytory {
+    @Override
+    public List<Customer> findAll() {
+        return null;
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void save(Customer model) {
+
+    }
+
+    @Override
+    public void remove(Long id) {
+
+    }
+}
--- src/main/java/com/codegym/cms/repository/impl/impl/CustomerReposytoryImpl.java
+++
@@ -1,6 +0,0 @@
-package com.codegym.cms.repository.impl.impl;
-
-import com.codegym.cms.repository.impl.CustomerReposytory;
-
-public class CustomerReposytoryImpl implements CustomerReposytory {
-}
